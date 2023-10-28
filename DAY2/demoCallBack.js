52c9fef17ce463b891cc26da5666a84eafab8562
tran-thien-nhan <pipclupnomad@gmail.com>
2023-10-28 14:28:56 +0700

    day 2

---
+++ DAY2/demoCallBack.js
@@ -0,0 +1,14 @@
+function taskOne(callback) {
+    setTimeout(() => {
+        // Biến data sẽ có giá trị sau 3 giây
+        const data = "this is my data";
+        callback(data);
+    }, 3000);
+}
+
+function taskTwo(value) {
+    console.log("data: ", value);
+}
+
+// Gọi taskOne và truyền callback taskTwo vào nó
+taskOne(taskTwo);
